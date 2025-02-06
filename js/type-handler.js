bb26405a25df3f6238607d6736023955b231df62
nivance <limingjian110@163.com>
2025-02-06 21:02:45 +0800

    Update type-handler.js

--- js/type-handler.js
+++ js/type-handler.js
@@ -3,10 +3,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const urlType = urlParams.get('type');
 const storedType = localStorage.getItem('type');
 
-console.log("urlType=" + urlType + "   storedType=" + storedType)
-
-
-let restartGame = false;
+let restart = false;
 // 确定使用哪个type值
 let type;
 if (!urlType) {
@@ -19,19 +16,31 @@ if (!urlType) {
     } else {
         // 检查请求的类型对应的文件夹是否存在
         const testImage = new Image();
+        testImage.onerror = () => { // asyn
+            type = 'president';         // default type
+            localStorage.setItem('type', type);
+            updateImages();
+            restartGame(true);
+        };
+        // testImage.onload = () => {
+        //     console.log("image loaded successfully");
+        // };
         testImage.src = `../collections/${urlType}/1.png`;
         
         type = urlType;
-        testImage.onerror = () => {
-            console.log("error type = " + type)
-            type = 'president';         // default type
-        };
-        restartGame = true;
+        restart = true;
     }
 }
 
 // 存储最终使用的type值
 localStorage.setItem('type', type);
+
+if (restart) {
+    restartGame(restart)
+}
+
+// 初始调用更新图片
+updateImages();
 
 // 更新图片路径的函数
 function updateImages() {
@@ -41,10 +50,11 @@ function updateImages() {
     }
 }
 
-console.log("updateImages ==== " + type + "  restartGame=" + restartGame)
-
-if (restartGame) {
-    console.log("restartGame=" + restartGame);
+function restartGame(restart) {
+    // console.log("type=" + type + "   restart=" + restart)
+    if (!restart) {
+        return
+    }
     // 等待 DOM 完全加载后再触发重启
     document.addEventListener("DOMContentLoaded", function() {
         setTimeout(() => {
@@ -64,5 +74,3 @@ if (restartGame) {
         }, 100); // 添加小延迟确保游戏完全初始化
     });
 }
-// 初始调用更新图片
-updateImages();
